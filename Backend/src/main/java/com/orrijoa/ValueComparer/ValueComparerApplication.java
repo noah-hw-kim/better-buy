c75a270e39cda692c56347e777af9f26fbd8a072
Noah Kim <noah.hw.kim@gmail.com>
2023-04-06 13:34:27 -0700

    created ValueComparer project with spring initializr and added backend

---
+++ Backend/src/main/java/com/orrijoa/ValueComparer/ValueComparerApplication.java
@@ -0,0 +1,13 @@
+package com.orrijoa.ValueComparer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValueComparerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValueComparerApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/orrijoa/ValueComparer/ValueComparerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orrijoa.ValueComparer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValueComparerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
