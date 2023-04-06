68b6a14609687e0766156618106c270c3fd7b468
Noah Kim <noah.hw.kim@gmail.com>
2023-04-06 15:58:00 -0700

    test Qudt java library to test unit conversion

--- Backend/src/main/java/com/orrijoa/ValueComparer/ValueComparerService.java
+++ Backend/src/main/java/com/orrijoa/ValueComparer/ValueComparerService.java
@@ -1,7 +1,10 @@
 package com.orrijoa.ValueComparer;
 
+import io.github.qudtlib.Qudt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 public class ValueComparerService {
@@ -9,15 +12,29 @@ public class ValueComparerService {
     @Autowired
     ValueComparerRepository valueComparerRepository;
 
+    /**
+     * 1. amount1 * unit1(standardized) * price1 compare
+     * */
     public Item getCheaper(Double amount1, String unit1, Double price1, Double amount2, String unit2, Double price2) {
         return null;
     }
 
-    private void standardizeByUnit() {
-
+    public Item test() {
+        System.out.println(Qudt.convert(new BigDecimal("500"), Qudt.Units.OZ, Qudt.Units.MilliT));
+        return null;
     }
 
+
+
+
+
+    /**
+     * compare Item 1 and Item 2's value and return the cheaper item
+     * */
     private Item compareValue(Item i1, Item i2) {
-        return null;
+        if (i1.compareTo(i2) > 0){
+            return i1;
+        }
+        return i2;
     }
 }
