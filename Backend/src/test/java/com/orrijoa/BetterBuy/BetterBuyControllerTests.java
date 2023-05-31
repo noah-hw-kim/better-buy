1d3b7dfe4cdc43719733791adbeb1cd58121f1cf
Noah Kim <noah.hw.kim@gmail.com>
2023-05-30 18:19:24 -0700

    Prepare Junit Test.
    Co-authored-by: Noah Kim <noah.hw.kim@gmail.com>
    Co-authored-by: Lanny Ngo <lannyngo@gmail.com>

--- Backend/src/test/java/com/orrijoa/BetterBuy/BetterBuyControllerTests.java
+++ Backend/src/test/java/com/orrijoa/BetterBuy/BetterBuyControllerTests.java
@@ -1,13 +1,14 @@
 package com.orrijoa.BetterBuy;
 
-import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
 
-@RunWith(MockitoJUnitRunner.class)
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class BetterBuyControllerTests {
 
+    @Test
     public void test(){
-
+        assertArrayEquals();
     }
 
 }
