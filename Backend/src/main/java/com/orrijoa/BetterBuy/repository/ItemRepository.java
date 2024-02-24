e37bbb190975f970fc1c4bf226e451dc3b4d3839
Noah Kim <noah.hw.kim@gmail.com>
2024-02-23 16:38:12 -0800

    try differnt approach on embedded mongodb test

--- Backend/src/main/java/com/orrijoa/BetterBuy/repository/ItemRepository.java
+++ Backend/src/main/java/com/orrijoa/BetterBuy/repository/ItemRepository.java
@@ -4,7 +4,10 @@ import com.orrijoa.BetterBuy.models.Item;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ItemRepository extends MongoRepository<Item, Long> {
     Item findItemById(long itemId);
+    List<Item> findItemsByName(String name);
 }
